5389307945c0ea8f989ba2bb5f0c8ee8bb2a375f
Solomon Tang <solomoncztang@gmail.com>
2019-07-02 13:06:36 -0700

    Declare WidgetProps

---
+++ src/widgets/types.js
@@ -0,0 +1,21 @@
+// @flow
+declare type WidgetProps = {
+  autofocus :boolean;
+  disabled :boolean;
+  formContext :Object;
+  id :string;
+  onBlur :(id :string, value :any) => void;
+  onChange :(value :any) => void;
+  onFocus :(id :string, value :any) => void;
+  options :Object;
+  readonly :boolean;
+  registry :Object;
+  required :boolean;
+  schema :Object;
+  type :string;
+  value :any;
+};
+
+export type {
+  WidgetProps
+};
