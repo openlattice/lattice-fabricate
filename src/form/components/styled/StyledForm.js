1bf158ada2ef497b5de046e9df1011ecc26245a3
Solomon Tang <solomoncztang@gmail.com>
2019-07-01 18:11:44 -0700

    Add StyledForm

---
+++ src/form/components/styled/StyledForm.js
@@ -0,0 +1,173 @@
+import styled from 'styled-components';
+import Form from 'react-jsonschema-form';
+
+const StyledForm = styled(Form)`
+  position: relative;
+  background-color: #fff;
+  border: solid 1px #e1e1eb;
+  border-radius: 5px;
+  margin: 20px 0;
+  /* styles for default nested elements provided by RJSF */
+
+  legend {
+    line-height: normal;
+    font-size: 16px;
+    grid-column: 1 / -1;
+  }
+
+  label.control-label {
+    display: inline-block;
+    color: #555e6f;
+    font-size: 14px;
+    line-height: normal;
+    margin-bottom: 10px;
+  }
+
+  /*
+    parent object is a grid container
+    cannot normally assign through uiSchema
+  */
+
+  > div.form-group.field.field-object > :first-child {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 0 30px;
+
+    & > * {
+      border-bottom: 1px solid #e1e1eb;
+      padding: 30px;
+    }
+
+    & > *:last-child {
+      border-bottom: 0;
+    }
+
+    & > legend {
+      line-height: normal;
+      font-size: 18px;
+      border-bottom: 0;
+      padding-bottom: 0;
+      margin: 0;
+    }
+  }
+
+  .column-span-1 {
+    grid-column: auto / span 1;
+  }
+
+  .column-span-2 {
+    grid-column: auto / span 2;
+  }
+
+  .column-span-3 {
+    grid-column: auto / span 3;
+  }
+
+  .column-span-4 {
+    grid-column: auto / span 4;
+  }
+
+  .column-span-5 {
+    grid-column: auto / span 5;
+  }
+
+  .column-span-6 {
+    grid-column: auto / span 6;
+  }
+
+  .column-span-7 {
+    grid-column: auto / span 7;
+  }
+
+  .column-span-8 {
+    grid-column: auto / span 8;
+  }
+
+  .column-span-9 {
+    grid-column: auto / span 9;
+  }
+
+  .column-span-10 {
+    grid-column: auto / span 10;
+  }
+
+  .column-span-11 {
+    grid-column: auto / span 11;
+  }
+
+  .column-span-12 {
+    grid-column: auto / span 12;
+  }
+
+  .grid-container {
+    display: flex;
+    width: 100%;
+
+    > :first-child {
+      display: grid;
+      grid-template-columns: repeat(12, 1fr);
+      grid-gap: 20px 30px;
+      width: 100%;
+    }
+
+    .divider {
+      margin-top: 15px;
+    }
+  }
+
+  .display-none {
+    display: none;
+  }
+
+  .grid-spread {
+    border-bottom: 1px solid #e1e1eb;
+
+    & > * {
+      padding: 5px;
+    }
+
+    :last-of-type {
+      border-bottom: 0;
+    }
+  }
+
+  center {
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+    align-items: center;
+  }
+
+  .section-spacing {
+    margin-top: 30px;
+  }
+
+  .hide-title {
+    > label {
+      display: none;
+    }
+  }
+
+  .compact-container {
+    > :first-child {
+      grid-gap: 10px 20px;
+    }
+  }
+
+  .subsection {
+    padding-bottom: 20px;
+    border-bottom: 1px #e1e1eb solid;
+
+    :last-of-type {
+      border-bottom: 0;
+      padding-bottom: 0;
+    }
+  }
+
+  .divider {
+    border-top: 1px solid #e1e1eb;
+    padding-top: 30px;
+  }
+`;
+
+export default StyledForm;
