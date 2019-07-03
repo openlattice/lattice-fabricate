1cabad17597959eac749a53ef7c2d05c8d2f1e3f
Solomon Tang <solomoncztang@gmail.com>
2019-07-03 16:12:53 -0700

    Add more cases to arraySchemas

--- src/form/stories/constants/arraySchemas.js
+++ src/form/stories/constants/arraySchemas.js
@@ -1,36 +1,128 @@
 export const schema = {
+  definitions: {
+    taskItems: {
+      type: 'object',
+      properties: {
+        taskName: {
+          type: 'string',
+          title: 'Task Name',
+          default: ''
+        },
+        taskDescription: {
+          type: 'string',
+          title: 'Task Description',
+          default: ''
+        }
+      }
+    }
+  },
   type: 'object',
-  title: 'Task List',
+  title: 'Arrays',
   properties: {
     tasks: {
       title: 'Tasks',
       type: 'array',
       items: {
-        type: 'object',
-        properties: {
-          taskName: {
-            type: 'string',
-            title: 'Task Name',
-            default: ''
-          },
-          taskDescription: {
-            type: 'string',
-            title: 'Task Description',
-            default: ''
-          }
-        }
+        $ref: '#/definitions/taskItems'
       },
       default: [{
         taskName: '',
         taskDescription: ''
       }]
     },
+    unorderable: {
+      title: 'Unorderable',
+      type: 'array',
+      items: {
+        $ref: '#/definitions/taskItems'
+      },
+      default: [{
+        taskName: '',
+        taskDescription: ''
+      }]
+    },
+    unremovable: {
+      title: 'Unremovable',
+      type: 'array',
+      items: {
+        $ref: '#/definitions/taskItems'
+      },
+      default: [{
+        taskName: '',
+        taskDescription: ''
+      }]
+    },
+    unindexed: {
+      title: 'Without Index',
+      type: 'array',
+      items: {
+        $ref: '#/definitions/taskItems'
+      },
+      default: [{
+        taskName: '',
+        taskDescription: ''
+      }]
+    }
   }
 };
 
 export const uiSchema = {
   tasks: {
     classNames: 'column-span-12',
+    'ui:options': {
+      addButtonText: '+ Add Task'
+    },
+    items: {
+      classNames: 'grid-container',
+      taskName: {
+        classNames: 'column-span-12'
+      },
+      taskDescription: {
+        classNames: 'column-span-12',
+        'ui:widget': 'textarea'
+      }
+    }
+  },
+  unorderable: {
+    classNames: 'column-span-12',
+    'ui:options': {
+      addButtonText: '+ Add Task',
+      orderable: false
+    },
+    items: {
+      classNames: 'grid-container',
+      taskName: {
+        classNames: 'column-span-12'
+      },
+      taskDescription: {
+        classNames: 'column-span-12',
+        'ui:widget': 'textarea'
+      }
+    }
+  },
+  unremovable: {
+    classNames: 'column-span-12',
+    'ui:options': {
+      addButtonText: '+ Add Task',
+      removable: false
+    },
+    items: {
+      classNames: 'grid-container',
+      taskName: {
+        classNames: 'column-span-12'
+      },
+      taskDescription: {
+        classNames: 'column-span-12',
+        'ui:widget': 'textarea'
+      }
+    }
+  },
+  unindexed: {
+    classNames: 'column-span-12',
+    'ui:options': {
+      addButtonText: '+ Add Task',
+      withIndex: false
+    },
     items: {
       classNames: 'grid-container',
       taskName: {
