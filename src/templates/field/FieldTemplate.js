12cd49adb9ceb0d751137ccad978ca8451e14b58
Solomon Tang <solomoncztang@gmail.com>
2019-07-08 15:44:49 -0700

    Render add custom FieldTemplate

--- src/templates/field/FieldTemplate.js
+++ src/templates/field/FieldTemplate.js
@@ -1,63 +1,84 @@
 // @flow
-import React from 'react';
-import { Label } from 'lattice-ui-kit';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Colors, Label } from 'lattice-ui-kit';
 import type { ChildrenArray, Element } from 'react';
+
+const { RED_1 } = Colors;
+
+const Error = styled.div`
+  color: ${RED_1};
+  font-size: 12px;
+  margin-top: 3px;
+`;
 
 type Props = {
   children :ChildrenArray<any>;
   classNames :string;
   description :Element<any>;
   displayLabel ? :boolean;
-  errors :Element<any>;
+  // errors :Element<any>;
   // fields :Object;
-  formContext :Object;
+  // formContext :Object;
   help :Element<any>;
   hidden ? :boolean;
   id :string;
   label :string;
   // rawDescription :string | Element<any>;
-  // rawErrors :string[];
+  rawErrors :string[];
   // rawHelp :string | Element<any>;
-  readonly ? :boolean;
+  // readonly ? :boolean;
   required ? :boolean;
 };
 
-const FieldTemplate = (props :Props) => {
-  const {
-    children,
-    classNames,
-    description,
-    displayLabel,
-    errors,
-    help,
-    hidden,
-    id,
-    label,
-    required,
-    ...restProps
-  } = props;
+class FieldTemplate extends Component<Props> {
 
-  if (hidden) {
-    return <div className="hidden">{children}</div>;
+  static defaultProps = {
+    displayLabel: true,
+    hidden: false,
+    required: false
+  };
+
+  renderErrors = () :Element<'div'>[] | null => {
+    const { rawErrors } = this.props;
+
+    if (Array.isArray(rawErrors)) {
+      return rawErrors.map(error => (
+        <Error>{error}</Error>
+      ));
+    }
+
+    return null;
   }
 
-  return (
-    <div className={classNames} {...restProps}>
-      {displayLabel && label && <Label className="control-label" htmlFor={id} required={required}>{label}</Label>}
-      {displayLabel && description ? description : null}
-      {description}
-      {children}
-      {errors}
-      {help}
-    </div>
-  );
-};
+  render() {
+    const {
+      children,
+      classNames,
+      description,
+      displayLabel,
+      help,
+      hidden,
+      id,
+      label,
+      required
+    } = this.props;
 
-FieldTemplate.defaultProps = {
-  displayLabel: true,
-  hidden: false,
-  readonly: false,
-  required: false
-};
+    if (hidden) {
+      return <div className="hidden">{children}</div>;
+    }
+
+    return (
+      <div className={classNames}>
+        {displayLabel && label && <Label htmlFor={id} required={required}>{label}</Label>}
+        {displayLabel && description ? description : null}
+        {description}
+        {children}
+        { this.renderErrors() }
+        {help}
+      </div>
+    );
+  }
+}
 
 export default FieldTemplate;
