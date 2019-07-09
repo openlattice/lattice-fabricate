bdf6f89d5d399440fe4ccb13fa04bfaf5cb2ee34
Solomon Tang <solomoncztang@gmail.com>
2019-07-09 16:28:06 -0700

    Remove unnecessary entityMapper

--- src/form/stories/FormContainer.js
+++ src/form/stories/FormContainer.js
@@ -4,12 +4,16 @@ import { DateTime } from 'luxon';
 
 import Form from '..';
 import { schema, uiSchema } from './constants/dataSchemas';
-import { getEntityAddressKey, INDEX_MAPPERS } from '../../utils/DataProcessingUtils';
+import {
+  processEntityData,
+  processAssociationEntityData
+} from '../../utils/DataProcessingUtils';
 import { ASSOCIATION_ENTITY_SET_NAMES, ENTITY_SET_NAMES, PROPERTY_TYPE_FQNS } from './constants/mockFQNs';
+import { entitySetIds, propertyTypeIds } from './constants/mockEDM';
 
 const { INCLUDES_ESN } = ASSOCIATION_ENTITY_SET_NAMES;
 const { TASK_LIST_ESN, TASK_ESN } = ENTITY_SET_NAMES;
-const { NAME_FQN, DESCRIPTION_FQN, COMPLETED_DT_FQN } = PROPERTY_TYPE_FQNS;
+const { COMPLETED_DT_FQN } = PROPERTY_TYPE_FQNS;
 
 type Props = {
 
@@ -20,17 +24,6 @@ type State = {
 };
 
 class FormContainer extends Component<Props, State> {
-
-  getEntityMappers = () => {
-    const indexMutator = (i :number) :number => i + 1;
-
-    const indexMappers = {
-      [getEntityAddressKey(-1, TASK_ESN, NAME_FQN)]: indexMutator,
-      [getEntityAddressKey(-1, TASK_ESN, DESCRIPTION_FQN)]: indexMutator
-    };
-
-    return { [INDEX_MAPPERS]: indexMappers };
-  }
 
   getAssociations = () => {
     const taskListEKID :string = '00000000-0000-0000-0000-000000000000';
@@ -44,6 +37,11 @@ class FormContainer extends Component<Props, State> {
 
   handleSubmit = ({ formData } :Object) => {
     console.log(formData);
+
+    const entityData = processEntityData(formData, entitySetIds, propertyTypeIds);
+    console.log(entityData);
+    // const associationData = processAssociationEntityData(this.getAssociations(), entitySetIds, propertyTypeIds);
+    // console.log(associationData);
   }
 
   render() {
