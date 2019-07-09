d36c4e264ce573e86a63f8b9982b9f063e54d3b6
Solomon Tang <solomoncztang@gmail.com>
2019-07-08 18:45:53 -0700

    Add DefinePlugin to storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,13 +5,25 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const Webpack = require('webpack');
 
-module.exports = function({ config }) {
+module.exports = function({ config, mode }) {
   config.module.rules.push({
     test: /\.stories.jsx?$/,
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
     enforce: 'pre',
   });
+
+  const ENV_DEV = 'development';
+  const ENV_PROD = 'production';
+
+  const DEFINE_PLUGIN = new Webpack.DefinePlugin({
+    __ENV_DEV__: JSON.stringify(mode === 'DEVELOPMENT'),
+    __ENV_PROD__: JSON.stringify(mode === 'PRODUCTION'),
+    __PACKAGE__: JSON.stringify('lattice-fabricate'),
+  });
+
+  config.plugins.push(DEFINE_PLUGIN);
 
   return config;
 };
