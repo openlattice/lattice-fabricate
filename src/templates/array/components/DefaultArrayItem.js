1b0a06ee0fb471203da4d8740a084c8bd3cc0251
Solomon Tang <solomoncztang@gmail.com>
2019-07-24 19:27:05 -0700

    Add submitbutton when adding item

--- src/templates/array/components/DefaultArrayItem.js
+++ src/templates/array/components/DefaultArrayItem.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { faChevronUp, faChevronDown } from '@fortawesome/pro-solid-svg-icons';
+import { Button } from 'lattice-ui-kit';
 import type { ChildrenArray } from 'react';
 
 import IconButton from '../../components/IconButton';
@@ -13,15 +14,21 @@ const ItemWrapper = styled.div`
   display: flex;
 `;
 
+const ButtonWithMargin = styled(Button)`
+  margin-left: ${props => props.orderable && '36px'};
+`;
+
 type Props = {
+  addAction ? :() => void;
+  addState ? :boolean;
   children :ChildrenArray<any>;
   className :string;
   disabled :boolean;
-  // formContext :Object;
   hasMoveDown :boolean;
   hasMoveUp :boolean;
   hasRemove :boolean;
   index :number;
+  isAdding ? :boolean;
   onDropIndexClick :(index :number) => Function;
   onReorderClick :(index :number, newIndex :number) => Function;
   orderable :boolean;
@@ -32,6 +39,9 @@ type Props = {
 class DefaultArrayItem extends Component <Props> {
 
   static defaultProps = {
+    addAction: undefined,
+    addState: false,
+    isAdding: false,
     showIndex: true,
   };
 
@@ -44,15 +54,31 @@ class DefaultArrayItem extends Component <Props> {
   }
 
   renderChildren = () => {
-    const { children, hasRemove } = this.props;
+    const { children, hasRemove, addState } = this.props;
 
     return React.Children.map(children, (child) => {
       return React.cloneElement(child, {
         ...child.props,
+        disabled: !addState,
+        hasRemove,
         onDelete: this.createDropIndexHandler(),
-        hasRemove
       });
     });
+  }
+
+  renderSubmitButton = () => {
+    const {
+      addState,
+      addAction,
+      isAdding,
+      orderable
+    } = this.props;
+
+    return addState && (
+      <ButtonWithMargin orderable={orderable} mode="primary" onClick={addAction} isLoading={isAdding}>
+          Submit
+      </ButtonWithMargin>
+    );
   }
 
   render() {
@@ -69,22 +95,25 @@ class DefaultArrayItem extends Component <Props> {
     } = this.props;
 
     return (
-      <ItemWrapper className={className}>
-        { orderable && (
-          <ActionGutter>
-            <IconButton
-                icon={faChevronUp}
-                disabled={disabled || readonly || !hasMoveUp}
-                onClick={onReorderClick(index, index - 1)} />
-            <IndexCircle index={index + 1} visible={showIndex} />
-            <IconButton
-                icon={faChevronDown}
-                disabled={disabled || readonly || !hasMoveDown}
-                onClick={onReorderClick(index, index + 1)} />
-          </ActionGutter>
-        )}
-        { this.renderChildren() }
-      </ItemWrapper>
+      <div>
+        <ItemWrapper className={className}>
+          { orderable && (
+            <ActionGutter>
+              <IconButton
+                  icon={faChevronUp}
+                  disabled={disabled || readonly || !hasMoveUp}
+                  onClick={onReorderClick(index, index - 1)} />
+              <IndexCircle index={index + 1} visible={showIndex} />
+              <IconButton
+                  icon={faChevronDown}
+                  disabled={disabled || readonly || !hasMoveDown}
+                  onClick={onReorderClick(index, index + 1)} />
+            </ActionGutter>
+          )}
+          { this.renderChildren() }
+        </ItemWrapper>
+        { this.renderSubmitButton() }
+      </div>
     );
   }
 }
