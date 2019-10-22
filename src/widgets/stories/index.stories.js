fa0f8e7de7eb114510fa33a66844f65f540d8398
Solomon Tang <solomoncztang@gmail.com>
2019-10-22 16:33:20 -0700

    Refactor Date/TimeWidget

--- src/widgets/stories/index.stories.js
+++ src/widgets/stories/index.stories.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { schema as numberSchema, uiSchema as numberUiSchema } from './constants/numbersSchemas';
 import { schema as datetimeSchema, uiSchema as datetimeUiSchema } from './constants/datetimeSchemas';
 
 import Form from '../../form';
+
+const dateTimeFormData = {
+  date: '2019-09-28',
+  time: '23:11'
+};
 
 storiesOf('Widgets', module)
   .add('Number', () => (
     <Form schema={numberSchema} uiSchema={numberUiSchema} />
   ))
   .add('Date & Time', () => (
-    <Form schema={datetimeSchema} uiSchema={datetimeUiSchema} />
+    <Form
+        formData={dateTimeFormData}
+        onSubmit={action('Submit')}
+        schema={datetimeSchema}
+        uiSchema={datetimeUiSchema} />
   ));
