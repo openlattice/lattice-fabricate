432dd3c79f2472bf7b56e86b48f457b651d43def
Solomon Tang <solomoncztang@gmail.com>
2020-01-22 12:02:36 -0800

    export XPage

--- src/form/index.js
+++ src/form/index.js
@@ -1,5 +1,6 @@
 import Form from './src/components/Form';
 import Paged from './src/components/Paged';
+import XPage from './src/components/XPage';
 
 export default Form;
-export { Paged };
+export { Paged, XPage };
