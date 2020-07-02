3f56198b6a3816b49b7fb3ba2a6ce77bb2ac7617
Solomon Tang <solomoncztang@gmail.com>
2020-07-01 19:13:15 -0700

    update label size and color

--- src/fields/DescriptionField.js
+++ src/fields/DescriptionField.js
@@ -5,9 +5,13 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import { Colors } from 'lattice-ui-kit';
+
+const { NEUTRAL } = Colors;
 
 const DescriptionDiv = styled.div`
   grid-column: 1 / -1;
+  color: ${NEUTRAL.N700};
   font-size: 14px;
   font-style: italic;
   margin-bottom: 5px;
