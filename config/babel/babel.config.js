cc197337a7a406290bd1dccefebafdc9810db207
Solomon Tang <solomoncztang@gmail.com>
2021-02-08 16:59:22 -0800

    update configs for new jsx transform

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -13,7 +13,9 @@ module.exports = {
     }]
   ],
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-react', {
+      runtime: 'automatic'
+    }],
     '@babel/preset-flow',
     '@babel/preset-react',
   ],
