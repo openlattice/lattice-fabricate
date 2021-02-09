1ace65f436aea392b73dff0d10af8af8c638f530
Solomon Tang <solomoncztang@gmail.com>
2021-02-08 17:04:15 -0800

    fix babel config

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -13,10 +13,10 @@ module.exports = {
     }]
   ],
   presets: [
+    '@babel/preset-env',
+    '@babel/preset-flow',
     ['@babel/preset-react', {
       runtime: 'automatic'
     }],
-    '@babel/preset-flow',
-    '@babel/preset-react',
   ],
 };
