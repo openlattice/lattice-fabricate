1172e5755be2f5e2277843169a9e97433d48f1b7
Solomon Tang <solomoncztang@gmail.com>
2021-05-07 18:57:16 -0700

    copy ObjectField

--- src/fields/index.js
+++ src/fields/index.js
@@ -4,5 +4,6 @@
 
 export { default as BulletField } from './BulletField';
 export { default as DescriptionField } from './DescriptionField';
+export { default as ObjectField } from './ObjectField';
 export { default as ParagraphField } from './ParagraphField';
 export { default as SchemaField } from './SchemaField';
