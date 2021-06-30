eb3e6450bd0573f93ff4b578596043139de04e1f
Solomon Tang <solomoncztang@gmail.com>
2021-06-29 20:55:52 -0700

    remove redundant ObjectField

--- src/fields/index.js
+++ src/fields/index.js
@@ -5,6 +5,5 @@
 export { default as AttachmentsField } from './AttachmentsField';
 export { default as BulletField } from './BulletField';
 export { default as DescriptionField } from './DescriptionField';
-export { default as ObjectField } from './ObjectField';
 export { default as ParagraphField } from './ParagraphField';
 export { default as SchemaField } from './SchemaField';
